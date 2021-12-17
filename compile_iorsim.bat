3b1f9ca16291118371def819202282303734680d
janlv <janludvig@gmail.com>
2021-12-17 16:11:32 +0100

    Delete compile_iorsim.bat

--- compile_iorsim.bat
+++
@@ -1,3 +0,0 @@
-C:\Users\javi\AppData\Local\Programs\Python\Python39\Scripts\pyinstaller.exe --onefile --add-data "guides;guides" --add-binary "iorsim_updater.exe;." --upx-dir C:\Users\javi\Downloads\upx-3.96-win64 .\IORSim_GUI.py 
-                
-                
