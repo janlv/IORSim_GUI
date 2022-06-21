51fe7291aa3401a578ddee99d618b1b9355e6b84
janlv <janludvig@gmail.com>
2022-06-21 19:38:27 +0200

    Delete test_timer.py

--- test_timer.py
+++
@@ -1,8 +0,0 @@
-from IORlib.utils import timer_thread
-from time import sleep
-
-with timer_thread(limit=5, prec=0.5, func=lambda : print('HEI')) as timer:
-    for i in range(10):
-        timer.start()
-        sleep(7)
-    timer.close()
