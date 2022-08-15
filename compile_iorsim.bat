070f457a81dcb97362bba49a8e51f3a2b207fac5
javi@osl-1011 <javi@norceresearch.no>
2022-08-15 16:34:08 +0200

    Updated build action

--- compile_iorsim.bat
+++ compile_iorsim.bat
@@ -2,4 +2,5 @@
 @REM set clean=""                
 @REM if "%~1"=="clean" set clean="--clean"
 
-C:\Users\javi\AppData\Local\Programs\Python\Python39\Scripts\pyinstaller.exe %~1 --onefile  --add-data "guides;guides" --add-data "icons;icons" --add-data "dist\upgrader\upgrader.exe;." --upx-dir "C:\Users\javi\Downloads\upx-3.96-win64" .\IORSim_GUI.py
+@REM C:\Users\javi\AppData\Local\Programs\Python\Python39\Scripts\pyinstaller.exe %~1 --onefile  --add-data "guides;guides" --add-data "icons;icons" --add-data "dist\upgrader\upgrader.exe;." --upx-dir "C:\Users\javi\Downloads\upx-3.96-win64" .\IORSim_GUI.py
+pyinstaller.exe %~1 --onefile  --add-data "guides;guides" --add-data "icons;icons" --upx-dir "upx-3.96-win64" .\IORSim_GUI.py
