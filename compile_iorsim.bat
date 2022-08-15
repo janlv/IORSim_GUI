9dde19ecc05efcbf756f67d59e90c926f2ca0a75
javi@osl-1011 <javi@norceresearch.no>
2022-08-15 17:20:02 +0200

    upx dir

--- compile_iorsim.bat
+++ compile_iorsim.bat
@@ -3,4 +3,4 @@
 @REM if "%~1"=="clean" set clean="--clean"
 
 @REM C:\Users\javi\AppData\Local\Programs\Python\Python39\Scripts\pyinstaller.exe %~1 --onefile  --add-data "guides;guides" --add-data "icons;icons" --add-data "dist\upgrader\upgrader.exe;." --upx-dir "C:\Users\javi\Downloads\upx-3.96-win64" .\IORSim_GUI.py
-pyinstaller.exe %~1 --onefile  --add-data "guides;guides" --add-data "icons;icons" --upx-dir "upx-3.96-win64" .\IORSim_GUI.py
+pyinstaller.exe %~1 --onefile  --add-data "guides;guides" --add-data "icons;icons" --upx-dir "upx-3.96-win64\upx-3.96-win64" .\IORSim_GUI.py
