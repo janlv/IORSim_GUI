3e5db09f07255f2d655baa5ed1b6c6301cf44309
janlv <janludvig@gmail.com>
2022-08-17 09:39:45 +0200

    Delete compile_iorsim.bat

--- compile_iorsim.bat
+++
@@ -1,6 +0,0 @@
-
-@REM set clean=""                
-@REM if "%~1"=="clean" set clean="--clean"
-
-@REM C:\Users\javi\AppData\Local\Programs\Python\Python39\Scripts\pyinstaller.exe %~1 --onefile  --add-data "guides;guides" --add-data "icons;icons" --add-data "dist\upgrader\upgrader.exe;." --upx-dir "C:\Users\javi\Downloads\upx-3.96-win64" .\IORSim_GUI.py
-pyinstaller.exe %~1 --onefile  --add-data "guides;guides" --add-data "icons;icons" --upx-dir "upx-3.96-win64" .\IORSim_GUI.py
