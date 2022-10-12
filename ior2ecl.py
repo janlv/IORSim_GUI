7adbe64aa7124c7aa616e640ba7d7c4d610afed7
Jan Ludvig Vinningland @ MBP13 <janludvig@gmail.com>
2022-10-12 08:57:00 +0200

    to_screen fix

--- ior2ecl.py
+++ ior2ecl.py
@@ -1295,13 +1295,17 @@ class Simulation:                                                        # Simul
     #--------------------------------------------------------------------------------
         ecl_unrst = UNRST_file(f'{self.root}_ECLIPSE.UNRST')
         ior = self.ior or Iorsim(root=self.root)   
-        with open(self.runlog.name, 'a') as self.runlog:   
+        try:
+            if self.runlog:
+                self.runlog = open(self.runlog.name, 'a')
+            #with open(self.runlog.name, 'a') as self.runlog:   
             self.print2log(f'\n Comparing {ecl_unrst.name()} and {ior.funrst.name()}:')
             for a,b in zip(ecl_unrst.data(*ecl_keys), ior.funrst.data(*ior_keys)):
                 self.print2log(f'  ECL: {print_dict(a)}')
                 self.print2log(f'  IOR: {print_dict(b)}')
                 self.print2log('')
-
+        finally:
+            self.runlog.close()
 
 
 
