fb0436db3f3b3199f8a795908e3511988feb24c6
Jan Ludvig Vinningland <javi@norceresearch.no>
2023-02-27 13:45:50 +0100

    v3.2

--- ior2ecl.py
+++ ior2ecl.py
@@ -24,7 +24,7 @@ from IORlib.runner import Runner
 from IORlib.ECL import (FUNRST_file, DATA_file, File, RFT_file, UNRST_file,
     UNSMRY_file, MSG_file, PRT_file)
 
-__version__ = '3.0.1'
+__version__ = '3.2'
 __author__ = 'Jan Ludvig Vinningland'
 
 DEBUG = False
